40e8b9094dd530df5e5fc9895446986ce3e6199b
nestormann <nestormann96@gamil.com>
2018-09-19 16:14:43 -0300

    mira la fecha

--- TP2/UART.v
+++ TP2/UART.v
@@ -3,68 +3,23 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 09/06/2017 04:36:02 PM
+// Create Date:    14:17:36 09/19/2018 
 // Design Name: 
-// Module Name: uart
+// Module Name:    UART 
 // Project Name: 
 // Target Devices: 
-// Tool Versions: 
+// Tool versions: 
 // Description: 
-// 
+//
 // Dependencies: 
-// 
-// Revision:
+//
+// Revision: 
 // Revision 0.01 - File Created
-// Additional Comments:
-// 
+// Additional Comments: 
+//
 //////////////////////////////////////////////////////////////////////////////////
+module UART(
+    );
 
-module UART
-#(
-	parameter NBIT_DATA = 8,
-	parameter NUM_TICKS = 16,
-	parameter BAUD_RATE = 9600,
-	parameter CLK_RATE = 50000000
-)
-(
-	input CLK, 	// clock de la FPGA
-	//input reset, // ver para queeeeeeeeeeeeeeee
-	input tx_start,
-	input rx_bit,
-	input [NBIT_DATA-1 : 0] data_in, // entrada del TX desde la interfaz (interfaz escribe en TX)
 
-	output [NBIT_DATA-1 : 0] data_out, // salida del RX que va hacia la interfaz (interfaz recibe de TX)
-	output rx_done_tick,
-	output tx_bit,
-	output tx_done_tick
-);
-
-	wire connect_baud_rate_rx_tx; 
-
-    BR_GEN #(.BAUD_RATE(BAUD_RATE), .CLK_RATE(CLK_RATE)) 
-    br_gen (
-    	.CLK(CLK),
-    	.TICK(connect_baud_rate_rx_tx));
-
-	RX #(.NBIT_DATA(NBIT_DATA), .NUM_TICKS(NUM_TICKS)) 
-    rx (
-		//.clk(CLK),
-		.rx_bit(rx_bit),
-		.tick(connect_baud_rate_rx_tx),
-
-		.rx_done_tick(rx_done_tick),
-		.data_out(data_out)
-		);
-
-    TX #(.NBIT_DATA(NBIT_DATA), .NUM_TICKS(NUM_TICKS)) 
-    tx (//.clk(CLK),
-		//.reset(reset),
-		.tx_start(tx_start),
-		.tick(connect_baud_rate_rx_tx),
-		.data_in(data_in),
-
-		.tx_done_tick(tx_done_tick),
-		.tx_bit(tx_bit)
-    	);
-endmodule	
-			
+endmodule
