bcd5073d88bcc354d1bbb061ce659591ec019548
guilletrejo <guille.trejo14@gmail.com>
2018-10-19 15:07:53 -0300

    cfds

--- TP3/uart.py
+++ TP3/uart.py
@@ -1,39 +1,17 @@
 import serial
 
-<<<<<<< HEAD
 ser = serial.Serial('/dev/ttyUSB0')
-=======
-ser = serial.Serial('COM6')
->>>>>>> 867004faa6ad069a8c0936b42da9a1c28230bcbc
 
 print ser.name
 
 # while True:
-<<<<<<< HEAD
-a = 0b00100000
+a = 0b00000001
 print a
 sent = ser.write(a)
 print "a =", bin(a)
 
-x = ser.read()       
+#x = ser.read()       
 
-print "result =", bin(ord(x))
-=======
-a = 0b01110000
-sent = ser.write(chr(a))
-print "a =", bin(a)
-
-b = 0b1
-sent = ser.write(chr(b))
-print "b =", bin(b)
-
-op = 0b100000
-sent = ser.write(chr(op))
-print "op =", bin(op)
-
-# x = ser.read()         
-
-# print "result =", bin(ord(x))
->>>>>>> 867004faa6ad069a8c0936b42da9a1c28230bcbc
+#print "result =", bin(ord(x))
 
 ser.close()
